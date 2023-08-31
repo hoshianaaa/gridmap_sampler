e722a2fb64bf57a7cdf03c5b0382663768239086
hoshianaaa <ryotaro.hoshina@gmail.com>
2023-08-31 13:45:08 +0900

    update

---
+++ src/demo1.cpp
@@ -0,0 +1,16 @@
+// 参考: https://github.com/ANYbotics/grid_map/blob/master/grid_map_demos/src/simple_demo_node.cpp
+
+#include <ros/ros.h>
+#include <grid_map_ros/grid_map_ros.hpp>
+#include <grid_map_msgs/GridMap.h>
+#include <cmath>
+
+using namespace grid_map;
+
+int main(int argc, char** argv)
+{
+
+    GridMap map({"elevation"});
+
+return 0;
+}
